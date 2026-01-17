595a0703d73772939117fad7418cd189d82b1cb1
Guilherme Jacob <gjsoaresc@gmail.com>
2026-01-17 11:17:01 -0300

    feat: remove docs

--- apps/docs/lib/source.ts
+++
@@ -1,8 +0,0 @@
-// Temporarily stub source to fix build
-// TODO: Fix fumadocs-mdx integration
-export const source = {
-  pageTree: { name: 'Docs', children: [] },
-  getPage: () => null,
-  getPages: () => [],
-  generateParams: () => [],
-}
--- apps/docs/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { createI18nMiddleware } from 'fumadocs-core/i18n/middleware'
-import { i18n } from '@/lib/i18n'
-
-export default createI18nMiddleware(i18n)
-
-export const config = {
-  // Skip all paths that should not be internationalized
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)'],
-}
